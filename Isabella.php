784342422cd6d18be870bd3b340f4767a041e5a0
Isabella2580 <isabellanaturinda5@gmail.com>
2024-09-19 18:08:32 +0300

    Isabella.php

---
+++ Isabella.php
@@ -0,0 +1,15 @@
+<?php
+$name="Naturinda Isabella.";
+$date="23/06/2002.";
+$address="Ruti-Mbarara.";
+echo"Name:".$name;
+echo"\nDate of birth:".$date;
+echo"\nAddress:".$address;
+echo"\n You are welcome home ".$name;
+$current=2024;
+$previous=2002;
+$Age=$current -$previous;
+echo " and you are now ".$Age."years old.";
+
+
+?>
